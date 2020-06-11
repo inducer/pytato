26b2dca73fbc58ab76af763b7c44b420d1c52cf1
Matt Wala <wala1@illinois.edu>
2020-06-12 00:03:54 +0200

    Apply suggestion to pytato/array.py

--- pytato/array.py
+++ pytato/array.py
@@ -442,6 +442,7 @@ class Array:
 
     @memoize_method
     def __hash__(self) -> int:
+        # TODO Cache computed hash value
         attrs = []
         for field in self.fields:
             attr = getattr(self, field)
