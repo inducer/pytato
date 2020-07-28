5581cdddd145eb46991503dc5b861a1ee0d655d2
Matt Wala <wala1@illinois.edu>
2020-07-28 07:39:14 +0200

    Apply 1 suggestion(s) to 1 file(s)

--- pytato/array.py
+++ pytato/array.py
@@ -1437,7 +1437,7 @@ def make_slice(array: Array, begin: Sequence[int], size: Sequence[int]) -> Array
         raise ValueError("'begin' and 'array' do not match in dimensions")
 
     if len(begin) != len(size):
-        raise ValueError("'begin' and 'size' do not match in dimensions")
+        raise ValueError("'begin' and 'size' do not match in number of dimensions")
 
     for i, (bval, sval) in enumerate(zip(begin, size)):
         symbolic_index = not (
