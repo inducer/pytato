bdd65e3192f477745fa474e48be07024ce9026ee
Andreas Klöckner <inform@tiker.net>
2021-01-08 18:17:31 -0600

    Add Taggable ref to Array docstring

--- pytato/array.py
+++ pytato/array.py
@@ -380,6 +380,8 @@ class Array(Taggable):
         <https://en.wikipedia.org/wiki/Resource_Description_Framework>`__
         triples (subject: implicitly the array being tagged,
         predicate: the tag, object: the arg).
+        
+        Inherits from :class:`pytools.Taggable`.
 
     .. automethod:: named
 
