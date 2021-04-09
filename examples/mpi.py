555035af984f011fe4e77022881a997daeb9c418
[6~ <inform@tiker.net>
2021-04-09 15:04:48 -0500

    Start SendFeederFinder in examples/mpi

--- examples/mpi.py
+++ examples/mpi.py
@@ -1,10 +1,33 @@
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
 import pytato as pt
+from pytato.transform import WalkMapper
 
 
 def advect(x, halo, bnd):
     return bnd*x*halo
+
+
+class SendFeederFinder(WalkMapper):
+    """
+    .. attribute:: node_to_fed_sends
+
+        :class:`dict`, maps each node in the graph to the set of sends that
+        depend on it
+    """
+    def __init__(self):
+        self.node_to_fed_sends = {}
+
+    def visit(self, expr, fed_sends):
+        self.node_to_fed_sends[expr] = fed_sends
+        return True
+
+    def map_distributed_send(self, expr, fed_sends):
+        fed_sends = fed_sends | {expr}
+        super().map_distributed_send(expr, fed_sends)
+
+    def __call__(self, expr):
+        return self.rec(expr, set())
 
 
 def main():
@@ -20,6 +43,11 @@ def main():
     y = advect(x, halo, bnd)
     #print(pt.generate_loopy({"y": y}).program)
 
+    sff = SendFeederFinder()
+    sff(y)
+
+    print(sff.node_to_fed_sends)
+    1/0
     dot_code = pt.get_dot_graph(y)
 
     from graphviz import Source
