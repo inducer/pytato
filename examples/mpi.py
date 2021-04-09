b3e902eca998bc75cf6f771cabf2cc69fc070133
[6~ <inform@tiker.net>
2021-04-09 14:24:04 -0500

    Hack dist example

--- examples/mpi.py
+++ examples/mpi.py
@@ -6,21 +6,27 @@ import pytato as pt
 def advect(x, halo, bnd):
     return bnd*x
 
-ns = pt.Namespace()
 
-rank = comm.Get_rank()
-size = comm.Get_size()
-x = pt.make_placeholder(ns, shape=(10,), dtype=float)
-bnd = pt.make_distributed_send(x[0], dest_rank=(rank-1) % size, comm_tag="halo")
+def main():
+    ns = pt.Namespace()
 
-halo = pt.make_distributed_recv(src_rank=(rank+1) % size, comm_tag="halo", shape=(),
-                          dtype=float, tags=pt.AdditionalOutput(bnd, prefix="send"))
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    x = pt.make_placeholder(ns, shape=(10,), dtype=float)
+    bnd = pt.make_distributed_send(x[0], dest_rank=(rank-1) % size, comm_tag="halo")
 
-y = advect(x, halo, bnd)
-print(pt.generate_loopy({"y": y}).program)
+    halo = pt.make_distributed_recv(src_rank=(rank+1) % size, comm_tag="halo",
+            shape=(), dtype=float, tags=pt.AdditionalOutput(bnd, prefix="send"))
 
-dot_code = pt.get_dot_graph(y)
+    y = advect(x, halo, bnd)
+    #print(pt.generate_loopy({"y": y}).program)
 
-from graphviz import Source
-src = Source(dot_code)
-src.view()
+    dot_code = pt.get_dot_graph(y)
+
+    from graphviz import Source
+    src = Source(dot_code)
+    src.view()
+
+
+if __name__ == "__main__":
+    main()
