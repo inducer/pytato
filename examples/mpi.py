8a0b92ff913cfcb810eab24628ba4710ae62b575
Matthias Diener <mdiener@illinois.edu>
2021-04-15 10:08:47 -0500

    partition

--- examples/mpi.py
+++ examples/mpi.py
@@ -4,8 +4,8 @@ import pytato as pt
 from pytato.transform import WalkMapper
 
 
-def advect(x, halo, bnd):
-    return bnd*x*halo
+def advect(*args):
+    return sum([a for a in args])
 
 
 class SendFeederFinder(WalkMapper):
@@ -20,7 +20,6 @@ class SendFeederFinder(WalkMapper):
 
     def visit(self, expr, fed_sends):
         self.node_to_fed_sends[expr] = fed_sends
-        print(expr, fed_sends)
         return True
 
     def map_distributed_send(self, expr, fed_sends):
@@ -43,14 +42,45 @@ def main():
     bnd = pt.make_distributed_send(x[0], dest_rank=(rank-1) % size, comm_tag="halo")
 
     halo = pt.make_distributed_recv(src_rank=(rank+1) % size, comm_tag="halo",
-            shape=(), dtype=float,
-            tags=frozenset({pt.AdditionalOutput(bnd, prefix="send")}))
+            shape=(), dtype=float)
+#            # tags=frozenset({pt.AdditionalOutput(bnd, prefix="send")}))
 
-    y = advect(x, halo, bnd)
+    y = x+bnd+halo
+
+    bnd2 = pt.make_distributed_send(y[0], dest_rank=(rank-1) % size, comm_tag="halo")
+
+    halo2 = pt.make_distributed_recv(src_rank=(rank+1) % size, comm_tag="halo",
+            shape=(), dtype=float)
+    y += bnd2 + halo2
+
+    # y = advect(x, halo, bnd)
     #print(pt.generate_loopy({"y": y}).program)
 
     sff = SendFeederFinder()
     sff(y)
+
+    # l_sends = set()
+
+    # for x in sff.node_to_fed_sends:
+    #     print(x, sff.node_to_fed_sends[x])
+    #     if len(sff.node_to_fed_sends[x]) > 0:
+    #         l_sends.add(sff.node_to_fed_sends[x].pop())
+
+    def partition_sends(node_to_fed_sends):
+        sends_to_nodes = {}
+
+        for x in node_to_fed_sends:
+            if len(sff.node_to_fed_sends[x]) > 0:
+                send = sff.node_to_fed_sends[x].pop()
+                sends_to_nodes.setdefault(send, []).append(x)
+
+        return sends_to_nodes
+        # print(sends_to_nodes)
+
+    print("========")
+    # print(l_sends)
+
+    print(partition_sends(sff.node_to_fed_sends))
 
     dot_code = pt.get_dot_graph(y)
 
