9a86654a5fe99fab83b6a3e5874018b34528bfd7
Matthias Diener <mdiener@illinois.edu>
2021-04-27 12:59:24 -0500

    running version

--- examples/mpi.py
+++ examples/mpi.py
@@ -1,26 +1,31 @@
 from mpi4py import MPI
 comm = MPI.COMM_WORLD
 import pytato as pt
-from pytato.transform import Mapper, WalkMapper, CopyMapper
+from pytato.transform import Mapper, CopyMapper
 
 from pytato.array import (Array, IndexLambda, DistributedRecv, DistributedSend,
-                          Placeholder)
+                          make_placeholder)
 
 from typing import Callable, Any
 
 
 def advect(*args):
+    """Test function."""
     return sum([a for a in args])
 
 
 class GraphToDictMapper(Mapper):
     """
+    Maps a graph to a dictionary representation.
+
     .. attribute:: graph_dict
 
         :class:`dict`, maps each node in the graph to the set of directly connected
         nodes, obeying the direction of each edge.
     """
-    def __init__(self):
+
+    def __init__(self) -> None:
+        """Initialize."""
         self.graph_dict = {}
 
     def map_placeholder(self, expr, *args):
@@ -86,88 +91,43 @@ def tag_nodes_with_starting_point(graph, node, starting_point=None, result=None)
     result.setdefault(node, set()).add(starting_point)
     if node in graph:
         for other_node_key in graph[node]:
-            tag_nodes_with_starting_point(graph, other_node_key, starting_point, result)
+            tag_nodes_with_starting_point(graph, other_node_key, starting_point,
+                                          result)
 
 
-class SendFeederFinder(WalkMapper):
-    """
-    .. attribute:: node_to_fed_sends
-
-        :class:`dict`, maps each node in the graph to the set of sends that
-        depend on it
-    """
-    def __init__(self):
-        self.node_to_fed_sends = {}
-
-    def visit(self, expr, fed_sends):
-        self.node_to_fed_sends[expr] = fed_sends
-        return True
-
-    def map_distributed_send(self, expr, fed_sends):
-        fed_sends = fed_sends | {expr}
-        super().map_distributed_send(expr, fed_sends)
-
-    def __call__(self, expr):
-        return self.rec(expr, set())
-
-
-class RecvFeedingFinder(WalkMapper):
-    """
-    .. attribute:: node_to_feeding_recvs
-
-        :class:`dict`, maps each node in the graph to the set of recv's that
-        feed it
-    """
-    def __init__(self):
-        self.node_to_feeding_recvs = {}
-
-    def visit(self, expr, receiving_nodes):
-        self.node_to_feeding_recvs[expr] = receiving_nodes
-        return True
-
-    def map_distributed_recv(self, expr, receiving_nodes):
-        receiving_nodes = receiving_nodes | {expr}
-        super().map_distributed_recv(expr, receiving_nodes)
-
-    def __call__(self, expr):
-        return self.rec(expr, set())
-
-
-def does_edge_cross_partition_boundary(expr1, expr2, node_to_fed_sends,
-                                       node_to_feeding_recvs) -> bool:
+def does_edge_cross_partition_boundary(node_to_fed_sends,
+                                       node_to_feeding_recvs, expr1, expr2) -> bool:
+    """Check if an edge crosses a partition boundary."""
     if (node_to_fed_sends[expr1] != node_to_fed_sends[expr2] or
              node_to_feeding_recvs[expr1] != node_to_feeding_recvs[expr2]):
-        print("partition", expr1, expr2)
+        print("CREATE partition", expr1, expr2)
         return True
     print("NO partition", expr1, expr2)
     return False
 
 
 class PartitionFinder(CopyMapper):
-    """
-    .. attribute:: node_to_feeding_recvs
+    """Find partitions."""
 
-        :class:`dict`, maps each node in the graph to the set of recv's that
-        feed it
-    """
-
-    def __init__(self, does_edge_cross_partition_boundary: Callable[[Any, Any], bool]):
-        # self.node_to_feeding_recvs = node_to_feeding_recvs
-        # self.node_to_fed_sends = node_to_fed_sends
+    def __init__(self, does_edge_cross_partition_boundary:
+                                   Callable[[Any, Any], bool]) -> None:
+        super().__init__()
         self.does_edge_cross_partition_boundary = does_edge_cross_partition_boundary
         self.cross_partition_name_to_value = {}
 
-    def check_partition(self, expr1, expr2):
-        if (self.node_to_fed_sends[expr1] != self.node_to_fed_sends[expr2] or
-             self.node_to_feeding_recvs[expr1] != self.node_to_feeding_recvs[expr2]):
-            print("partition", expr1, expr2)
-            return True
-        print("NO partition", expr1, expr2)
-        return False
+        self.name_index = 0
+
+    def make_new_name(self):
+        self.name_index += 1
+        res = "placeholder_" + str(self.name_index)
+        assert res not in self.cross_partition_name_to_value
+        return res
+
+    def check_partition(self, expr1, expr2) -> bool:
+        return self.does_edge_cross_partition_boundary(expr1, expr2)
 
     def map_distributed_send(self, expr, *args):
         self.check_partition(expr, expr.data)
-
         self.rec(expr.data)
 
     def map_distributed_recv(self, expr, *args):
@@ -175,33 +135,51 @@ class PartitionFinder(CopyMapper):
         self.rec(expr.data)
 
     def map_slice(self, expr, *args):
-        self.check_partition(expr, expr.array)
-        self.rec(expr.array)
+        new_bindings = {}
+        name = self.make_new_name()
+        if self.check_partition(expr, expr.array):
+            new_bindings[name] = make_placeholder(expr.array.shape, expr.array.dtype,
+                                                  name, tags=expr.array.tags)
+            self.cross_partition_name_to_value[name] = self.rec(expr.array)
+        else:
+            new_bindings[name] = self.rec(expr.array)
 
     def map_placeholder(self, expr, *args):
+        new_bindings = {}
         for dim in expr.shape:
             if isinstance(dim, Array):
-                self.check_partition(expr, dim)
-                self.rec(dim, *args)
+                name = self.make_new_name()
+                if self.check_partition(expr, dim):
+                    new_bindings[name] = make_placeholder(dim.shape, dim.dtype, name,
+                                                          tags=dim.tags)
+                    self.cross_partition_name_to_value[name] = self.rec(dim)
+                else:
+                    new_bindings[name] = self.rec(dim)
 
     def map_index_lambda(self, expr: IndexLambda, *args) -> None:
         new_bindings = {}
-        for name, child in expr.bindings.values():
+        for child in expr.bindings.values():
+            name = self.make_new_name()
             if self.check_partition(expr, child):
-                name = self.make_new_name()
-                assert name not in self.cross_partition_name_to_value
-                new_bindings[name] = Placeholder(name)
+
+                new_bindings[name] = make_placeholder(child.shape, child.dtype, name,
+                                                      tags=child.tags)
                 self.cross_partition_name_to_value[name] = self.rec(child)
             else:
                 new_bindings[name] = self.rec(child)
 
         for dim in expr.shape:
             if isinstance(dim, Array):
-                self.check_partition(expr, dim)
-                self.rec(dim)
+                name = self.make_new_name()
+                if self.check_partition(expr, dim):
+                    new_bindings[name] = make_placeholder(dim.shape, dim.dtype, name,
+                                                          tags=dim.tags)
+                    self.cross_partition_name_to_value[name] = self.rec(dim)
+                else:
+                    new_bindings[name] = self.rec(dim)
 
     def __call__(self, expr):
-        return self.rec(expr, set())
+        return self.rec(expr)
 
 # Partition the pytato graph whenever an edge (node1, node2) satisfies
 # node_to_fed_sends[node1] != node_to_fed_sends[node2] or
@@ -238,21 +216,6 @@ def main():
             shape=(), dtype=float)
     y += bnd2 + halo2
 
-    if 0:
-        sff = SendFeederFinder()
-        sff(y)
-
-        for k, v in sff.node_to_fed_sends.items():
-            print(k, v)
-
-        print("========")
-
-        rff = RecvFeedingFinder()
-        rff(y)
-
-        for k, v in rff.node_to_feeding_recvs.items():
-            print(k, v)
-
     gdm = GraphToDictMapper()
     gdm(y)
 
@@ -265,44 +228,27 @@ def main():
     # FIXME: Inefficient... too many traversals
     node_to_feeding_recvs = {}
     for node in graph:
+        node_to_feeding_recvs.setdefault(node, set())
         if isinstance(node, DistributedRecv):
             tag_nodes_with_starting_point(graph, node, result=node_to_feeding_recvs)
 
     node_to_fed_sends = {}
     for node in rev_graph:
+        node_to_fed_sends.setdefault(node, set())
         if isinstance(node, DistributedSend):
             tag_nodes_with_starting_point(rev_graph, node, result=node_to_fed_sends)
 
+    from functools import partial
+    pfunc = partial(does_edge_cross_partition_boundary, node_to_fed_sends,
+                    node_to_feeding_recvs)
+
+    pf = PartitionFinder(pfunc)
+    pf(y)
+
+    show_graph(y)
+
     print("========")
-    print(node_to_feeding_recvs)
-    print("========")
-    print(node_to_fed_sends)
-
-    # for k, v in gdm.graph_dict.items():
-    #     print(k, v)
-    # print(gdm.graph_dict)
-
-    # pf = PartitionFinder(sff.node_to_fed_sends, rff.node_to_feeding_recvs)
-    # pf(y)
-
-    def partition_sends(node_to_fed_sends):
-        sends_to_nodes = {}
-
-        for x in node_to_fed_sends:
-            if len(sff.node_to_fed_sends[x]) > 0:
-                send = next(iter(sff.node_to_fed_sends[x]))
-                sends_to_nodes.setdefault(send, []).append(x)
-
-        return sends_to_nodes
-
-    # print("========")
-    # print(partition_sends(sff.node_to_fed_sends))
-
-    dot_code = pt.get_dot_graph(y)
-
-    from graphviz import Source
-    src = Source(dot_code)
-    src.view()
+    print(pf.cross_partition_name_to_value)
 
 
 if __name__ == "__main__":
