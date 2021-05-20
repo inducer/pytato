bd7840ca7fc0ea7460c3a025f28b4d0b58b385a4
Matthias Diener <matthias.diener@gmail.com>
2021-05-20 16:59:04 -0500

    Update pytato/array.py
    Co-authored-by: Andreas Klöckner <inform@tiker.net>

--- pytato/array.py
+++ pytato/array.py
@@ -1821,7 +1821,9 @@ def _normalize_reduction_axes(
         shape: ShapeType,
         reduction_axes: Optional[Union[int, Tuple[int]]]
         ) -> Tuple[ShapeType, Tuple[int, ...]]:
-    """Removes the reduction axes from shape and normalizes it to a tuple."""
+    """Accepts the *shape* of the array to be reduced over, removes the (given) *reduction_axes*
+    from shape and normalizes the result to be a tuple.
+    """
     if reduction_axes is None:
         return (), tuple(range(len(shape)))
 
