81da218c0a33e7ef35f8b01f4cf7a31cccc45544
Kaushik Kulkarni <kaushikcfd@gmail.com>
2021-06-23 23:37:21 -0500

    loopy always support callables now

--- test/test_codegen.py
+++ test/test_codegen.py
@@ -47,14 +47,6 @@ from testlib import assert_allclose_to_numpy
 import pymbolic.primitives as p
 
 
-def does_lpy_support_knl_callables():
-    try:
-        from loopy import TranslationUnit  # noqa: F401
-        return True
-    except ImportError:
-        return False
-
-
 def test_basic_codegen(ctx_factory):
     ctx = ctx_factory()
     queue = cl.CommandQueue(ctx)
@@ -681,8 +673,6 @@ def test_maximum_minimum(ctx_factory, which):
     np.testing.assert_allclose(y, np_func(x1_in, x2_in), rtol=1e-6)
 
 
-@pytest.mark.skipif(not does_lpy_support_knl_callables(), reason="loopy does not"
-        " support calling kernels")
 def test_call_loopy(ctx_factory):
     from pytato.loopy import call_loopy
     cl_ctx = ctx_factory()
@@ -705,8 +695,6 @@ def test_call_loopy(ctx_factory):
     assert (z_out == 40*(x_in.sum(axis=1))).all()
 
 
-@pytest.mark.skipif(not does_lpy_support_knl_callables(), reason="loopy does not"
-        " support calling kernels")
 def test_call_loopy_with_same_callee_names(ctx_factory):
     from pytato.loopy import call_loopy
 
@@ -748,8 +736,6 @@ def test_exprs_with_named_arrays(ctx_factory):
     np.testing.assert_allclose(out, 42*x_in)
 
 
-@pytest.mark.skipif(not does_lpy_support_knl_callables(), reason="loopy does not"
-        " support calling kernels")
 def test_call_loopy_with_parametric_sizes(ctx_factory):
 
     x_in = np.random.rand(10, 4)
@@ -776,8 +762,6 @@ def test_call_loopy_with_parametric_sizes(ctx_factory):
     np.testing.assert_allclose(z_out, 42*(x_in.sum(axis=1)))
 
 
-@pytest.mark.skipif(not does_lpy_support_knl_callables(), reason="loopy does not"
-        " support calling kernels")
 def test_call_loopy_with_scalar_array_inputs(ctx_factory):
     import loopy as lp
     from numpy.random import default_rng
