e682649fe37b41bb35ca3ca05434141faaa47c55
Matthias Diener <mdiener@illinois.edu>
2021-08-30 13:05:07 -0500

    cleanup example

--- examples/partition.py
+++ examples/partition.py
@@ -6,8 +6,6 @@ import numpy as np
 from pytato.transform import (GraphToDictMapper, TopoSortMapper, PartitionId,
                               find_partitions)
 
-# from pytato.visualization import show_dot_graph
-
 from dataclasses import dataclass
 
 
@@ -33,42 +31,15 @@ def main():
     tm = TopoSortMapper()
     tm(y)
 
-    print(tm.topological_order)
-
     from functools import partial
     pfunc = partial(get_partition_id, tm.topological_order)
 
-    # graph = gdm.graph_dict
-    # rev_graph = reverse_graph(graph)
-
-    # # FIXME: Inefficient... too many traversals
-    # node_to_feeding_recvs = {}
-    # for node in graph:
-    #     node_to_feeding_recvs.setdefault(node, set())
-    #     # FIXME: IndexLambda is just a place where the graph should be split:
-    #     if isinstance(node, IndexLambda):
-    #        tag_nodes_with_starting_point(graph, node, result=node_to_feeding_recvs)
-
-    # node_to_fed_sends = {}
-    # for node in rev_graph:
-    #     node_to_fed_sends.setdefault(node, set())
-    #     # FIXME: IndexLambda is just a place where the graph should be split:
-    #     if isinstance(node, IndexLambda):
-    #        tag_nodes_with_starting_point(rev_graph, node, result=node_to_fed_sends)
-
-    # from functools import partial
-    # pfunc = partial(get_partition_id, node_to_fed_sends,
-    #                 node_to_feeding_recvs)
-
-    # print(f"{graph=}")
-    # print(f"{node_to_feeding_recvs=}")
-    # print(f"{node_to_fed_sends=}")
-
+    # Find the partitions
     (toposorted_partitions, prg_per_partition,
     partition_id_to_input_names, partition_id_to_output_names) \
         = find_partitions(y, pfunc)
 
-    # execution
+    # Execute the partitions
     ctx = cl.create_some_context()
     queue = cl.CommandQueue(ctx)
 
@@ -79,18 +50,16 @@ def main():
         for k in partition_id_to_input_names[pid]:
             if k in context:
                 inputs[k] = context[k]
-        # prg_per_partition[f](**inputs)
         res = prg_per_partition[pid](**inputs)
 
         context.update(res[1])
 
+    # Execute unpartitioned code for comparison
     prg = pt.generate_loopy(y)
     evt, (out, ) = prg(queue)
 
-    # print(out)
 
     final_res = context[partition_id_to_output_names[toposorted_partitions[-1]][0]]
-    # print(final_res)
 
     assert np.allclose(out, final_res)
 
