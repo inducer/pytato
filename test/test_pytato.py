382e9cf33223ba7aeca76d294670cde9079bcfb0
Matthias Diener <mdiener@illinois.edu>
2021-11-02 11:46:42 -0500

    add random DAG test

--- test/test_pytato.py
+++ test/test_pytato.py
@@ -305,20 +305,24 @@ def test_toposortmapper():
 
 
 def test_userscollector():
-    n = pt.make_size_param("n")
-    array = pt.make_placeholder(name="array", shape=n, dtype=np.float64)
-    stack = pt.stack([array, 2*array, array + 6])
-    y = stack @ stack.T
-
+    from testlib import RandomDAGContext, make_random_dag
     from pytato.transform import UsersCollector, reverse_graph
 
-    gdm = UsersCollector()
-    gdm(y)
+    axis_len = 5
 
-    rev_graph = reverse_graph(gdm.graph_dict)
-    rev_graph2 = reverse_graph(reverse_graph(rev_graph))
+    for i in range(100):
+        rdagc = RandomDAGContext(np.random.default_rng(seed=i),
+                axis_len=axis_len, use_numpy=False)
 
-    assert rev_graph2 == rev_graph
+        dag = make_random_dag(rdagc)
+
+        gdm = UsersCollector()
+        gdm(dag)
+
+        rev_graph = reverse_graph(gdm.graph_dict)
+        rev_graph2 = reverse_graph(reverse_graph(rev_graph))
+
+        assert rev_graph2 == rev_graph
 
 
 def test_asciidag():
