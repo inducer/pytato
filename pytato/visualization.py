9d6e5ec20cbcef6da6ad09318ae15e84dce4c472
Matthias Diener <mdiener@illinois.edu>
2021-11-08 13:31:54 -0600

    flake8

--- pytato/visualization.py
+++ pytato/visualization.py
@@ -322,7 +322,7 @@ def get_dot_graph_from_partitions(parts: CodePartitions) -> str:
 
                         # Emit cross-partition edges
                         assert array.name
-  
+
                         tgt = array_to_id[parts.var_name_to_result[array.name]]
                         emit(f"{tgt} -> {array_to_id[array]} [style=dashed]")
 
