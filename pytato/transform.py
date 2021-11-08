4d2f2347910f1270d6030a8a782ee8a74334533d
Matthias Diener <mdiener@illinois.edu>
2021-11-08 13:28:14 -0600

    fix reverse_graph

--- pytato/transform.py
+++ pytato/transform.py
@@ -1131,6 +1131,10 @@ def reverse_graph(graph: Dict[ArrayOrNames, Set[ArrayOrNames]]) \
     result: Dict[ArrayOrNames, Set[ArrayOrNames]] = {}
 
     for node_key, edges in graph.items():
+        # Make sure every node is in the result even if it has no users
+        if node_key not in result:
+            result[node_key] = set()
+
         for other_node_key in edges:
             result.setdefault(other_node_key, set()).add(node_key)
 
