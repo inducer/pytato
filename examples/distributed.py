ba374bc91c60cb33374e4aa7034024a8f18818b1
Matthias Diener <mdiener@illinois.edu>
2021-11-08 14:01:45 -0600

    fix recv hang
    Previously, send was stapled to recv, and both were in the same
    partition. This meant that first the irecv was waited on before the
    corresponing send, leading to a deadlock.

--- examples/distributed.py
+++ examples/distributed.py
@@ -43,10 +43,12 @@ def main():
     x = pt.make_data_wrapper(x_in)
 
     halo = staple_distributed_send(x, dest_rank=(rank-1) % size, comm_tag=42,
-            stapled_to=make_distributed_recv(
-                src_rank=(rank+1) % size, comm_tag=42, shape=(4, 4), dtype=int))
+            stapled_to=x)
 
-    y = x+halo
+    bnd = make_distributed_recv(
+                src_rank=(rank+1) % size, comm_tag=42, shape=(4, 4), dtype=int)
+
+    y = x+halo+bnd
 
     # bnd2 = pt.make_distributed_send(y[0], dest_rank=(rank-1)%size, comm_tag="halo")
 
