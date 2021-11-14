3640ababe5ab70fd99a2ec4103befb605c9dc908
Matthias Diener <mdiener@illinois.edu>
2021-11-14 16:41:09 -0600

    add missing map_loopy_call

--- pytato/transform.py
+++ pytato/transform.py
@@ -1296,6 +1296,16 @@ class EdgeCachedMapper(CachedMapper[ArrayOrNames], ABC):
         assert expr.name
         return SizeParam(name=expr.name, tags=expr.tags)
 
+    def map_loopy_call(self, expr: LoopyCall) -> LoopyCall:
+        return LoopyCall(
+            translation_unit=expr.translation_unit,
+            entrypoint=expr.entrypoint,
+            bindings={
+                name: self.handle_edge(expr, child)
+                if isinstance(child, Array) else child
+                for name, child in expr.bindings.items()},
+            )
+
     # }}}
 
 # }}}
