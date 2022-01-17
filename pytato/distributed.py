4a4b2aeb77226e28b099b29d53237b3f56fc1851
Matthias Diener <mdiener@illinois.edu>
2022-01-17 16:47:46 -0600

    cleanup distributed.py imports

--- pytato/distributed.py
+++ pytato/distributed.py
@@ -24,8 +24,8 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
-from typing import (Any, Dict, Hashable, Tuple, Optional, Set, # noqa (need List, FrozenSet, Mapping for sphinx)
-    List, FrozenSet, Mapping, Callable, cast)
+from typing import (Any, Dict, Hashable, Tuple, Optional, Set,
+    List, FrozenSet, Callable, cast)
 
 from dataclasses import dataclass
 
