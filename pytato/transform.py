a752793db09a22a9d103a362d038479d28b25a4a
Matthias Diener <mdiener@illinois.edu>
2022-04-06 23:40:36 -0500

    add PooledBuffer to dedup cache key

--- pytato/transform.py
+++ pytato/transform.py
@@ -1717,9 +1717,11 @@ def _get_data_dedup_cache_key(ary: DataInterface) -> Hashable:
     if "pyopencl" in sys.modules:
         from pyopencl.array import Array as CLArray  # type: ignore[import]
         from pyopencl import Buffer as CLBuffer
+        from pyopencl._cl import PooledBuffer
         if isinstance(ary, CLArray):
             # pyopencl represents 0-long arrays' base_data as None
-            assert isinstance(ary.base_data, CLBuffer) or (ary.base_data is None)
+            assert (isinstance(ary.base_data, (PooledBuffer, CLBuffer))
+                    or (ary.base_data is None))
             return (
                     None if ary.base_data is None else ary.base_data.int_ptr,
                     ary.offset,
