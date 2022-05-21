423e3fb3aaa6950d458b360bf984f13e5bac8c1c
Matthias Diener <mdiener@illinois.edu>
2022-05-21 18:10:24 -0500

    remove duplicated self.axes in hash()

--- pytato/array.py
+++ pytato/array.py
@@ -1598,7 +1598,7 @@ class DataWrapper(InputArgumentBase):
     def __hash__(self) -> int:
         from pytato.equality import preprocess_tags_for_equality
         return hash((self.name, id(self.data), self._shape, self.axes,
-                     preprocess_tags_for_equality(self.tags), self.axes))
+                     preprocess_tags_for_equality(self.tags)))
 
     def __eq__(self, other: Any) -> bool:
         return self is other
