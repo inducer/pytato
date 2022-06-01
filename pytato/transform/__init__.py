262787c68c32e799ced65c0d3eb1440ce2a50c83
Matthias Diener <mdiener@illinois.edu>
2022-06-01 11:42:26 -0500

    use DEBUG_ENABLED

--- pytato/transform/__init__.py
+++ pytato/transform/__init__.py
@@ -1300,9 +1300,11 @@ def materialize_with_mpms(expr: DictOfNamedArrays) -> DictOfNamedArrays:
 
     res = DictOfNamedArrays(new_data)
 
-    logger.info("materialize_with_mpms: materialized "
-        f"{get_num_tags_of_type(res, ImplStored())} out of "
-        f"{get_num_nodes(res)} nodes")
+    from pytato import DEBUG_ENABLED
+    if DEBUG_ENABLED:
+        logger.info("materialize_with_mpms: materialized "
+            f"{get_num_tags_of_type(res, ImplStored())} out of "
+            f"{get_num_nodes(res)} nodes")
 
     return res
 
