d3c64296ce4a24cb3edfc130e21f3f620c5410fc
Matthias Diener <mdiener@illinois.edu>
2022-10-31 19:58:44 -0500

    add mpi4py intersphinx

--- doc/conf.py
+++ doc/conf.py
@@ -29,6 +29,7 @@ intersphinx_mapping = {
     "https://pyrsistent.readthedocs.io/en/latest/": None,
     "https://jax.readthedocs.io/en/latest/": None,
     "https://www.attrs.org/en/stable/": None,
+    "https://mpi4py.readthedocs.io/en/latest": None,
 }
 
 # Some modules need to import things just so that sphinx can resolve symbols in
