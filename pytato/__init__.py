a01789910b4f79fcd5bf7b42296dee76b41ac91a
Kaushik Kulkarni <kaushikcfd@gmail.com>
2022-11-04 23:27:58 -0500

    Introduce `pt.trace_call`
    Co-authored-by: Andreas Kloeckner <andreask@illinois.edu>

--- pytato/__init__.py
+++ pytato/__init__.py
@@ -95,6 +95,7 @@ from pytato.visualization import (get_dot_graph, show_dot_graph,
                                   )
 import pytato.analysis as analysis
 import pytato.tags as tags
+import pytato.function as function
 import pytato.transform as transform
 from pytato.distributed.nodes import (make_distributed_send, make_distributed_recv,
                                 DistributedRecv, DistributedSend,
@@ -111,6 +112,7 @@ from pytato.transform.lower_to_index_lambda import to_index_lambda
 from pytato.transform.remove_broadcasts_einsum import (
     rewrite_einsums_with_no_broadcasts)
 from pytato.transform.metadata import unify_axes_tags
+from pytato.function import trace_call
 
 __all__ = (
         "dtype",
@@ -156,6 +158,8 @@ __all__ = (
 
         "broadcast_to", "pad",
 
+        "trace_call",
+
         "make_distributed_recv", "make_distributed_send", "DistributedRecv",
         "DistributedSend", "staple_distributed_send", "DistributedSendRefHolder",
 
@@ -175,6 +179,6 @@ __all__ = (
         "unify_axes_tags",
 
         # sub-modules
-        "analysis", "tags", "transform",
+        "analysis", "tags", "transform", "function",
 
 )
