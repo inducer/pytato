edf82b3922632fec5e80c582c27efbe4d7bcb42a
Matthew Smith <mjsmith6@illinois.edu>
2022-11-28 12:29:31 -0600

    post receives even if len(partition.parts) == 1
    a part might not do any work before receiving

--- pytato/distributed/execute.py
+++ pytato/distributed/execute.py
@@ -88,22 +88,16 @@ def execute_distributed_partition(
 
     from mpi4py import MPI
 
-    if len(partition.parts) != 1:
-        recv_names_tup, recv_requests_tup, recv_buffers_tup = zip(*[
-            (name,) + _post_receive(mpi_communicator, recv)
-            for part in partition.parts.values()
-            for name, recv in part.input_name_to_recv_node.items()])
-        recv_names = list(recv_names_tup)
-        recv_requests = list(recv_requests_tup)
-        recv_buffers = list(recv_buffers_tup)
-        del recv_names_tup
-        del recv_requests_tup
-        del recv_buffers_tup
-    else:
-        # Only a single partition, no recv requests exist
-        recv_names = []
-        recv_requests = []
-        recv_buffers = []
+    recv_names_tup, recv_requests_tup, recv_buffers_tup = zip(*[
+        (name,) + _post_receive(mpi_communicator, recv)
+        for part in partition.parts.values()
+        for name, recv in part.input_name_to_recv_node.items()])
+    recv_names = list(recv_names_tup)
+    recv_requests = list(recv_requests_tup)
+    recv_buffers = list(recv_buffers_tup)
+    del recv_names_tup
+    del recv_requests_tup
+    del recv_buffers_tup
 
     context: Dict[str, Any] = input_args.copy()
 
