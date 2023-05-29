37cbe3115dcfd78e190d72fda4f796a95ee839e9
Andreas Kloeckner <inform@tiker.net>
2023-05-29 17:32:46 -0500

    Drop asciidag visualization

--- pytato/visualization/__init__.py
+++ pytato/visualization/__init__.py
@@ -2,19 +2,15 @@
 .. currentmodule:: pytato
 
 .. automodule:: pytato.visualization.dot
-.. automodule:: pytato.visualization.ascii
 .. automodule:: pytato.visualization.fancy_placeholder_data_flow
 """
 
 from .dot import get_dot_graph, show_dot_graph, get_dot_graph_from_partition
-from .ascii import get_ascii_graph, show_ascii_graph
 from .fancy_placeholder_data_flow import show_fancy_placeholder_data_flow
 
 
 __all__ = [
     "get_dot_graph", "show_dot_graph", "get_dot_graph_from_partition",
-
-    "get_ascii_graph", "show_ascii_graph",
 
     "show_fancy_placeholder_data_flow",
 ]
