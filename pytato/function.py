4e2aae7d79ef4b242d5b41c0d519197fc0da8075
Matthew Smith <mjsmith6@illinois.edu>
2024-03-12 10:02:39 -0500

    memoize NamedCallResult creation

--- pytato/function.py
+++ pytato/function.py
@@ -314,6 +314,7 @@ class Call(AbstractResultWithNamedArrays):
     def __iter__(self) -> Iterator[str]:
         return iter(self.function.returns)
 
+    @memoize_method
     def __getitem__(self, name: str) -> NamedCallResult:
         return NamedCallResult(
             self, name,
