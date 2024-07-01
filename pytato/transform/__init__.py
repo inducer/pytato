2caecbb2588c2b66bc5c914dcac6c0811559702e
Matthew Smith <mjsmith6@illinois.edu>
2024-07-01 14:53:03 -0500

    enable collision/duplication checks by default

--- pytato/transform/__init__.py
+++ pytato/transform/__init__.py
@@ -204,7 +204,7 @@ class CachedMapper(Mapper, Generic[CachedMapperT]):
     .. automethod:: clone_for_callee
     """
 
-    def __init__(self, err_on_collision: bool = False) -> None:
+    def __init__(self, err_on_collision: bool = True) -> None:
         super().__init__()
         self._err_on_collision = err_on_collision
         self._seen_exprs: Dict[Hashable, ArrayOrNames] = {}
@@ -263,7 +263,7 @@ class TransformMapper(CachedMapper[ArrayOrNames]):
 
     def __init__(
             self,
-            err_on_collision: bool = False,
+            err_on_collision: bool = True,
             err_on_duplication: Optional[bool] = None) -> None:
         """
         :arg err_on_collision: Raise an exception if two distinct input array
@@ -357,7 +357,7 @@ class TransformMapperWithExtraArgs(CachedMapper[ArrayOrNames]):
 
     def __init__(
             self,
-            err_on_collision: bool = False,
+            err_on_collision: bool = True,
             err_on_duplication: Optional[bool] = None) -> None:
         """
         :arg err_on_collision: Raise an exception if two distinct input array
