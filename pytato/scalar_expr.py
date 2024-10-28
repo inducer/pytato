a0da6dc9e02c3d1759b65084f379e993e3e4b52c
Andreas Klöckner <inform@tiker.net>
2024-10-28 14:24:09 -0500

    Improve cast-to-bool style

--- pytato/scalar_expr.py
+++ pytato/scalar_expr.py
@@ -349,5 +349,5 @@ def contains_reduction(expr: prim.Expression) -> bool:
     Returns true if any operation in the scalar expression, expr, is a reduction
     operation.
     """
-    return (len(get_reduction_induction_variables(expr)) > 0)
+    return bool(get_reduction_induction_variables(expr))
 # vim: foldmethod=marker
