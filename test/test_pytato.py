8df0687036ff98a8f50a981e4cc273a9d1aeed49
Matthias Diener <mdiener@illinois.edu>
2024-11-25 14:43:13 -0600

    add another test

--- test/test_pytato.py
+++ test/test_pytato.py
@@ -1381,6 +1381,9 @@ def test_numpy_type_promotion_with_pytato_arrays():
 
 def test_pickling_hash():
     # See https://github.com/inducer/pytato/pull/563 for context
+
+    # {{{ Placeholder
+
     p = pt.make_placeholder("p", (4, 4), int)
 
     assert not hasattr(p, "_hash_value")
@@ -1395,6 +1398,32 @@ def test_pickling_hash():
     p_new = loads(dumps(p))
 
     assert not hasattr(p_new, "_hash_value")
+
+    assert p == p_new
+
+    # }}}
+
+    # {{{ DataWrapper
+
+    dw = pt.make_data_wrapper(np.zeros((4, 4), int))
+
+    assert not hasattr(dw, "_hash_value")
+
+    hash(dw)
+
+    # DataWrappers have no hash caching
+    assert not hasattr(dw, "_hash_value")
+
+    dw_new = loads(dumps(dw))
+
+    assert dw_new.shape == dw.shape
+    assert dw_new.dtype == dw.dtype
+    assert np.all(dw_new.data == dw.data)
+
+    # DataWrappers that are not the same object compare unequal
+    assert dw != dw_new
+
+    # }}}
 
 
 if __name__ == "__main__":
