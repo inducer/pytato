ddad374560d58050b5c63e4e2e8cca24870e80a0
Addison Alvey-Blanco <aja13@illinois.edu>
2024-12-13 11:28:16 -0600

    fix ruff complaints

--- test/test_pytato.py
+++ test/test_pytato.py
@@ -1325,8 +1325,8 @@ def test_unify_axes_tags():
 
 
 def test_ignoring_axes_during_propagation():
-    from pytools.tag import UniqueTag
     from pytato.transform.metadata import AxisIgnoredForPropagationTag
+    from pytools.tag import UniqueTag
 
     class ElementAxisTag(UniqueTag):
         pass
