190cb68725582f6a3bdec71d914f3368d1550cea
Matthew Smith <mjsmith6@illinois.edu>
2025-02-07 16:16:55 -0600

    add CacheInputs to simplify cache key handling logic

--- pytato/analysis/__init__.py
+++ pytato/analysis/__init__.py
@@ -622,9 +622,9 @@ class TagCountMapper(CombineMapper[int, Never]):
         return sum(args)
 
     def rec(self, expr: ArrayOrNames) -> int:
-        key = self._cache.get_key(expr)
+        inputs = self._make_cache_inputs(expr)
         try:
-            return self._cache.retrieve(expr, key=key)
+            return self._cache.retrieve(inputs)
         except KeyError:
             s = Mapper.rec(self, expr)
             if (
@@ -636,7 +636,7 @@ class TagCountMapper(CombineMapper[int, Never]):
             else:
                 result = 0 + s
 
-            self._cache.add(expr, 0, key=key)
+            self._cache.add(inputs, 0)
             return result
 
 
