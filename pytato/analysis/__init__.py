2571a89494b9efa4d479052e15acd5236edc6094
Matthew Smith <mjsmith6@illinois.edu>
2025-12-05 15:33:20 -0600

    clarify how flop counting functions behave w.r.t. DAG functions

--- pytato/analysis/__init__.py
+++ pytato/analysis/__init__.py
@@ -1068,6 +1068,10 @@ def get_num_flops(
         For arrays whose index lambda form contains :class:`pymbolic.primitives.If`,
         this function assumes a SIMT-like model of computation in which the per-entry
         cost is the sum of the costs of the two branches.
+
+    .. note::
+
+        This *does* descend into functions.
     """
     from pytato.codegen import normalize_outputs
     expr = normalize_outputs(expr)
@@ -1101,6 +1105,10 @@ def get_materialized_node_flop_counts(
         For arrays whose index lambda form contains :class:`pymbolic.primitives.If`,
         this function assumes a SIMT-like model of computation in which the per-entry
         cost is the sum of the costs of the two branches.
+
+    .. note::
+
+        This *does not* descend into functions.
     """
     from pytato.codegen import normalize_outputs
     expr = normalize_outputs(expr)
@@ -1138,6 +1146,10 @@ def get_unmaterialized_node_flop_counts(
         For arrays whose index lambda form contains :class:`pymbolic.primitives.If`,
         this function assumes a SIMT-like model of computation in which the per-entry
         cost is the sum of the costs of the two branches.
+
+    .. note::
+
+        This *does not* descend into functions.
     """
     from pytato.codegen import normalize_outputs
     expr = normalize_outputs(expr)
